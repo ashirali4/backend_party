e4c983d2ed42955fe884afbe639f8fee11af231a
ashir.muhammad <itxaxhu668@gmail.com>
2021-02-09 17:06:19 +0500

    Changed name

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OpenTable',
+      title: 'Bday Party',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
